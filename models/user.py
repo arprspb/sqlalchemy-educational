c19b141d12b8b7fba93859a46436d0ced054c212
= <arprspb@gmail.com>
2024-02-24 06:18:23 +0300

    drawio extension test

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 from asyncio import FastChildWatcher
 from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import Relationship
 from models.base import TimeStampedModel
 
 
@@ -11,6 +12,9 @@ class User(TimeStampedModel):
     last_name = Column(String(80), nullable=False)
     email = Column(String(320), nullable=False, unique = True)
 
+    preference = Relationship("Preference", back_populates="user", uselist=False,
+                              passive_deletes=True
+                              )
 
     def __repr__(self):
         return f"{self.__class__.__name__}, name: {self.first_name} {self.last_name}"
@@ -22,4 +26,6 @@ class Preference(TimeStampedModel):
     id = Column(Integer, primary_key=True, autoincrement=True)
     language = Column(String(80), nullable=False)
     currency = Column(String(80), nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False, index=True, unique=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True, unique=True)
+
+    user = Relationship("User", back_populates="preference")
